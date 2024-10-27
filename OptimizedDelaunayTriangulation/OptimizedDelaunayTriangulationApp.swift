0f45be4b3e5f190b4fc6cba5db06517e46018e31
Paul Ollivier <p0l@me.com>
2024-10-27 17:15:35 +0100

    Initial Commit

---
+++ OptimizedDelaunayTriangulation/OptimizedDelaunayTriangulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OptimizedDelaunayTriangulationApp.swift
+//  OptimizedDelaunayTriangulation
+//
+//  Created by Paul Ollivier on 27/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct OptimizedDelaunayTriangulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
